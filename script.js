61e628abeb526cf481b8f754713f603d69ebc4c4
alfaro-matttthew <137105768+alfaro-matttthew@users.noreply.github.com>
2023-06-28 19:41:21 -0400

    wrote out all of the variables, prompts, and confirms. I wrote a function to determine if the length is correct and a placeholder for a function to build the password

--- script.js
+++ script.js
@@ -1,74 +1,62 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-//hit btn prompt how many chars
-
-//Validation > if less than 8 or more than 128 alert and restart
-
-//if between 8 and 128 confirm lowercase
-//confitm uppercase
-//confirm numeric
-//confirm special characters
-
-//if yes add to array
-
-//print password to screen
-
-//////////////////////////////
-
-// const NumArrary = [1,2,3,4,5,6,7,8,9,0]
-// const special = ["!", "#", "$", "%", "&","*", "?", "@", "^"];
-// const tallLetter = capitolized letters
-// const lowLetter = lowercase letters
-
-//promts for password length, cap, lower, special, numbers
-
-//if (passObj.count < 8 || passObj.count > 128 ) {
-//   alert("That number is not between 8 and 128.  Please enter a correct number.");
-
-//function buildPassword() {
-
-// if want caps then concat caps to password
-
-//console log password to confirm caps have been added
-
-// if want lower then concat lower to password
-
-//else {
-//  passObj.upper = confirm("Tall Letters?");
-
 const lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 const upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 const numberChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 const specialChar = ["!", "#", "$", "%", "&","*", "?", "@", "^"];
+var passwordPool = []
 
-console.log(lowerCase, upperCase, numberChar, specialChar)
+let passLength = prompt('How many characters does your password need to have?', 'Must be between 8 and 128 characters long');
+let passLowerCase = confirm("Do you want to use lower case letters in your password?");
+let passUpperCase = confirm("Do you want to use UPPER CASE in your password?");
+let passNumber = confirm("Do you want to use numbers in your password?");
+let passSpecialChar = confirm("Do you want to use special characters in your password?");
+
+       function lengthPrompt() {
+          if (passLength > 8 && passLength < 128) {
+            lengthValue = true;
+          }
+          else {
+            alert("Password Length Must Be Between 8 And 128 Characters Long!")
+          }
+        }
+
+        function buildPassword() {
+
+        }
+
+// let isBoss = confirm("Are you the boss?");
+// alert(isBoss);
+
+// console.log(lengthPrompt)
+
+// window.prompt("How many characters must be included in the password?", "8 to 128 Characters Allowed");
+
+// // Assignment Code
+// var generateBtn = document.querySelector("#generate");
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
 
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//   passwordText.value = password;
 
-  passwordText.value = password;
+// }
 
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
