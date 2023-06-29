80439baa23949fd6feda8ff3ae134f13f8471394
alfaro-matttthew <137105768+alfaro-matttthew@users.noreply.github.com>
2023-06-28 23:39:11 -0400

    set up all fo the if functinos for all of the possible characters for the password generator

--- script.js
+++ script.js
@@ -3,19 +3,12 @@
 
 const lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 const upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-const numberChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-const specialChar = ["!", "#", "$", "%", "&","*", "?", "@", "^"];
-var indexPool = []
-var passwordPool = []
+const numberChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const specialChar = ['!', '#', '$', '%', '&', '*', '?', '@', '^'];
+var indexPool = [];
+var passwordPool = [];
 
-// let passLength = prompt('How many characters does your password need to have?', 'Must be between 8 and 128 characters long');
-// let passLowerCase = confirm("Do you want to use lower case letters in your password?");
-// let passUpperCase = confirm("Do you want to use UPPER CASE in your password?");
-// let passNumber = confirm("Do you want to use numbers in your password?");
-// let passSpecialChar = confirm("Do you want to use special characters in your password?");
-
-
-//--------WORKSPACE WORKSPACE WORKSPACE----------------------------------------------------------//
+// STEP ONE//
 
 let passLength = prompt('How many characters does your password need to have?', 'Must be between 8 and 128 characters long');
 
@@ -27,6 +20,44 @@ let passLength = prompt('How many characters does your password need to have?', 
           alert("Password Length Must Be Between 8 And 128 Characters Long!")
         } ;
 
+// STEP TWO //
+
+let passLowerCase = confirm("Do you want to use lower case letters in your password?");
+
+        //If the confirm is true, then lower case letters are added to the indexPool//
+        if (passLowerCase = true) {
+          var passwordPool = lowerCase.concat(passwordPool);
+        }
+
+// STEP THREE //
+
+let passUpperCase = confirm("Do you want to use UPPER CASE in your password?");
+
+        //If the confirm is true, then upper case letters are added to the indexPool//
+        if (passUpperCase = true) {
+          var passwordPool = upperCase.concat(passwordPool);
+        }
+
+// STEP FOUR //
+
+let passNumber = confirm("Do you want to use numbers in your password?");
+
+        //If the confirm is true, then numbers are added to the indexPool//
+        if (passNumber = true) {
+          var passwordPool = numberChar.concat(passwordPool);
+        }
+
+// STEP FIVE //
+
+let passSpecialChar = confirm("Do you want to use special characters in your password?");
+
+        //If the confirm is true, then special characters are added to the indexPool//
+        if (passSpecialChar = true) {
+          var passwordPool = specialChar.concat(passwordPool);
+        }
+
+// STEP SIX //
+
         //If the lenght is correct, then it invokes the setLength function//
         if (lengthValue === true) {
           setLength();
@@ -36,116 +67,22 @@ let passLength = prompt('How many characters does your password need to have?', 
         //These numbers will be used to select random strings from the passwordPool array//
         function setLength() {
           for (let step = 0; step < passLength; step++)
-            indexPool.push(Math.floor(Math.random()*128)+8);
+            indexPool.push(Math.floor(Math.random()*120)+8);
+            makePassword();
         }
 
         console.log(indexPool);
 
+        function makePassword() {
+          for (let step = 0; step < passLength; step++)
+          
+          console.log('this is working');
+        }
+
+        console.log(passwordPool);
+
 
 //--------WORKSPACE WORKSPACE WORKSPACE----------------------------------------------------------//
-
-// //--------Length Prompt Function-----------------------------------------------------------------//
-
-
-//        function lengthPrompt() {
-//           if (passLength > 8 && passLength < 128) {
-//             lengthValue = true;
-//           }
-//           else {
-//             alert("Password Length Must Be Between 8 And 128 Characters Long!")
-//           }
-//         }
-
-//         lengthPrompt();
-//         console.log(lengthValue);
-
-// //--------Length Prompt Function-----------------------------------------------------------------//        
-
-// //--------Lower Case Function--------------------------------------------------------------------//
-
-
-//         function chooseLowerCase() {
-//           if (passLowerCase === true) {
-//             passwordPool.push(lowerCase);
-//             console.log(passwordPool);
-//           }
-//           else {
-            
-//           }
-//         }
-        
-//         chooseLowerCase();
-
-// //--------Lower Case Function--------------------------------------------------------------------//
-
-// //--------Upper Case Function--------------------------------------------------------------------//
-
-
-//         function chooseUpperCase() {
-//           if (passUpperCase === true) {
-//             passwordPool.push(upperCase);
-//             console.log(passwordPool);
-//           }
-//           else {
-            
-//           }
-//         }
-        
-//         chooseUpperCase();
-
-// //--------Upper Case Function--------------------------------------------------------------------//
-
-// //--------Number Function------------------------------------------------------------------------//
-
-
-//         function chooseNumber() {
-//           if (passNumber === true) {
-//             passwordPool.push(numberChar);
-//             console.log(passwordPool);
-//           }
-//           else {
-            
-//           }
-//         }
-
-//         chooseNumber();
-
-// //--------Number Function------------------------------------------------------------------------//
-
-// //--------Special Character Function------------------------------------------------------------//
-
-
-//         function chooseSpecial() {
-//           if (passSpecialChar === true) {
-//             passwordPool.push(specialChar);
-//             console.log(passwordPool);
-//           }
-//           else {
-            
-//           }
-//         }
-
-//         chooseSpecial();
-
-// //--------Special Character Function------------------------------------------------------------//
-
-//         function lowerCaseConfirm() {
-
-//         }
-
-//         function buildPassword() {
-//           if (lengthValue = true) {
-//             passwordPool.push()
-//           }
-//         }
-
-// //--------Special Character Function------------------------------------------------------------//
-
-// //--------Random Number Method-----------------------------------------------------------------//
-
-//         Math.floor(Math.random() * 128) + 8;
-
-// //--------Random Number Method-----------------------------------------------------------------//
 
 
 // let isBoss = confirm("Are you the boss?");
